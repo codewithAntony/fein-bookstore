cf17bdb51a2753b58568c5b19a0585e4d2ca71c6
Antony Murithi <antonymurithi51@gmail.com>
2024-06-13 18:35:04 +0300

    Login button and logic

--- django_project/urls.py
+++ django_project/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
+    path('admin/', admin.site.urls), #Django admin
+    path('accounts/', include('django.contrib.auth.urls')), #user management
+    path('', include('pages.urls')), #local apps
 ]
